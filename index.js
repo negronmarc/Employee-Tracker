71d93e4607fa09c116aa863993865fc5a80f0ccc
Marc Negron <negronmarc@gmail.com>
2022-01-09 01:21:50 -0500

    added readme

--- index.js
+++ index.js
@@ -29,26 +29,26 @@ async function loadMainPrompts() {
           name: "View All Employees By Department",
           value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
         },
-        {
-          name: "View All Employees By Manager",
-          value: "VIEW_EMPLOYEES_BY_MANAGER"
-        },
+        // {
+        //   name: "View All Employees By Manager",
+        //   value: "VIEW_EMPLOYEES_BY_MANAGER"
+        // },
         {
           name: "Add Employee",
           value: "ADD_EMPLOYEE"
         },
-        {
-          name: "Remove Employee",
-          value: "REMOVE_EMPLOYEE"
-        },
+        // {
+        //   name: "Remove Employee",
+        //   value: "REMOVE_EMPLOYEE"
+        // },
         {
           name: "Update Employee Role",
           value: "UPDATE_EMPLOYEE_ROLE"
         },
-        {
-          name: "Update Employee Manager",
-          value: "UPDATE_EMPLOYEE_MANAGER"
-        },
+        // {
+        //   name: "Update Employee Manager",
+        //   value: "UPDATE_EMPLOYEE_MANAGER"
+        // },
         {
           name: "View All Roles",
           value: "VIEW_ROLES"
@@ -57,10 +57,10 @@ async function loadMainPrompts() {
           name: "Add Role",
           value: "ADD_ROLE"
         },
-        {
-          name: "Remove Role",
-          value: "REMOVE_ROLE"
-        },
+        // {
+        //   name: "Remove Role",
+        //   value: "REMOVE_ROLE"
+        // },
         {
           name: "View All Departments",
           value: "VIEW_DEPARTMENTS"
@@ -69,10 +69,10 @@ async function loadMainPrompts() {
           name: "Add Department",
           value: "ADD_DEPARTMENT"
         },
-        {
-          name: "Remove Department",
-          value: "REMOVE_DEPARTMENT"
-        },
+        // {
+        //   name: "Remove Department",
+        //   value: "REMOVE_DEPARTMENT"
+        // },
         {
           name: "Quit",
           value: "QUIT"
